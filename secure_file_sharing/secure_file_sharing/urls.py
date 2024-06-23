c01d66aa34686b861367a76ad2de75bf94894395
Devraj Chhikara <97741424+devrajchhikara@users.noreply.github.com>
2024-06-23 14:41:06 +0000

    f2

--- secure_file_sharing/secure_file_sharing/urls.py
+++ secure_file_sharing/secure_file_sharing/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', admin.site.urls),  # Redirect to Django admin
     path('admin/', admin.site.urls),
     path('api/', include('file_management.urls')),
 ]
